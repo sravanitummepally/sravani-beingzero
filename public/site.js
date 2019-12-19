e2b873d8032d9133e902dd089d264c0b9322fe66
Sravani <sravanitummepally123@gmail.com>
2019-12-19 13:36:34 +0530

    final one

--- public/site.js
+++ public/site.js
@@ -3,7 +3,6 @@ const app = express()
 const port = process.env.PORT || 3000
 app.use(express.static('public'))
 
-app.get('/', (req, res) => res.sendFile(__dirname + '/public/html/index.html'))
-
+app.get('/', (req, res) => res.sendFile(__dirname + '/public/htmls/index.html'))
 
 app.listen(port, () => console.log(`Example app listening on port ${port}!`))
